16b31c4d585298ac7ddc13626ed93314ea79a7c8
Raymond Wang <ray2019imac@Raymonds-iMac-2.local>
2024-01-01 22:52:41 -0800

    Added temporary request access functionality

--- src/components/RequestAccess.js
+++ src/components/RequestAccess.js
@@ -1,6 +1,27 @@
 import React, {useState} from 'react'
+import axios from 'axios'
 
 const RequestAccess = () => {
+  const [first, setFirst] = useState('')
+  const [last, setLast] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = {
+      First: first,
+      Last: last,
+      Email: email
+    }
+    console.log(data)
+    axios.post('https://sheet.best/api/sheets/d62da5e3-fc37-4faa-951a-a688dddb5daa', data)
+      .then((response) => {
+        setFirst('')
+        setLast('')
+        setEmail('')
+      })
+  }
+
   return (
     <section className='section'>
       <div className="container mx-auto lg:mt-24">
@@ -11,10 +32,31 @@ const RequestAccess = () => {
           <p className="text-gray-700 mt-8 lg:mt-4 max-w-[800px]">
             Sign up to turn your medical reports into accessible, interactive and jargon-less summaries.
           </p>
-          <form action="" className="space-y-8 w-full max-w-[780px] flex flex-col items-center text-center mt-16">
-            <input type="text" className='input' placeholder='Your First Name' required />
-            <input type="text" className='input' placeholder='Your Last Name' required />
-            <input type="email" className='input' placeholder='Your E-Mail' required />
+          <form action="" className="space-y-8 w-full max-w-[780px] flex flex-col items-center text-center mt-16" onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              className='input' 
+              placeholder='Your First Name' 
+              required
+              onChange={(e) => setFirst(e.target.value)}
+              value={first}
+            />
+            <input 
+              type="text" 
+              className='input' 
+              placeholder='Your Last Name' 
+              required
+              onChange={(e) => setLast(e.target.value)}
+              value={last}
+            />
+            <input 
+              type="email" 
+              className='input' 
+              placeholder='Your E-Mail' 
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              value={email} 
+            />
             <button className="btn btn-md bg-accent hover:bg-accent-hover transition-all duration-300">
               Request Access
             </button>
