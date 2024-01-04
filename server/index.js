f50bb3565cddb550330a6a3dd74ead315e5404cb
Raymond Wang <ray2019imac@Raymonds-iMac-2.local>
2024-01-04 00:54:21 -0800

    Created build path

--- server/index.js
+++ server/index.js
@@ -5,9 +5,25 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-const PORT = process.env.PORT || 5500
 
+const PORT = process.env.PORT || 5500
+const path = require('path')
 app.use(cors())
+
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname, "../build")
+
+app.use(express.static(buildPath))
+app.get("/*", function(req, res) {
+  res.sendFile(
+    path.join(__dirname, "../build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    }
+  )
+})
 
 const UserRoute = require('./routes/users')
 
